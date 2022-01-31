116ef84332935837fb06a38098583ba242e33dd6
KushalGhimire17 <kushalghimire61@gmail.com>
2022-01-31 16:32:19 +0545

    [Added] templates directory and simple home_page

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import User, Lead, Agent
 
-# Register your models here.
+admin.site.register([User, Lead, Agent])
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Agent, Lead
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, "home_page.html")
