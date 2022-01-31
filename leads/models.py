080eba60d0f05b65c55b6db3c938243dbcb8c96a
KushalGhimire17 <kushalghimire61@gmail.com>
2022-01-31 16:06:46 +0545

    [Created] Superuser and [Added] dummy data through querysets and model manager

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,3 @@
-from importlib.machinery import SOURCE_SUFFIXES
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -7,15 +6,23 @@ class User(AbstractUser):
     # to add custom fields in the future
     pass
 
+# Many Lead has One Agent
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
-    # every lead has an agent
+    # every lead has an agent."Agent" means Agent class is in this file but doesn't follow topdown approach
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Agent(models.Model):
     # every agent has one user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
