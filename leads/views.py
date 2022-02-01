8df0f6e92f1fd41959cb69030b92fbf639365062
KushalGhimire17 <kushalghimire61@gmail.com>
2022-02-01 17:49:31 +0545

    [Created] Lead List and Lead Detail Page

--- leads/views.py
+++ leads/views.py
@@ -4,4 +4,20 @@ from .models import Agent, Lead
 
 
 def home_page(request):
-    return render(request, "home_page.html")
+    return render(request, 'home_page.html')
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
